66f7c060d146315ba08b7d7372989cf55ff92f81
heninth <heninth@hen.in.th>
2016-09-07 23:55:16 +0700

    change app name

--- config/app.php
+++ config/app.php
@@ -12,7 +12,7 @@ return [
     | any other location as required by the application or its packages.
     */
 
-    'name' => 'My Application',
+    'name' => 'Stock Simulation Game for CSS 361',
 
     /*
     |--------------------------------------------------------------------------
