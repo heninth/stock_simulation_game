8519bd37c766db02e7cc516ab07fbb2f5a3a1593
heninth <heninth@hen.in.th>
2016-09-11 19:27:53 +0700

    function key - get value

--- app/Setting.php
+++ app/Setting.php
@@ -6,5 +6,32 @@ use Illuminate\Database\Eloquent\Model;
 
 class Setting extends Model
 {
-    //
+    /**
+     * Model's primary key.
+     *
+     * @var string
+     */
+    public $primaryKey = 'key';
+    public $incrementing = false;
+
+    /**
+     * Indicates if the model should be timestamped.
+     *
+     * @var bool
+     */
+    public $timestamps = false;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'key', 'value',
+    ];
+
+    public static function key($key)
+    {
+        return Setting::firstOrCreate(['key' => $key])->value;
+    }
 }
