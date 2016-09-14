7aea13bbd14dc55db0d4febe12294057f91dd208
heninth <heninth@hen.in.th>
2016-09-14 16:24:00 +0700

    home list my stock

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+use App\User;
+use App\UserPort;
+use App\StockSymbol;
 
 class HomeController extends Controller
 {
@@ -23,6 +28,9 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $user = User::find(Auth::user()->id);
+        $stocks = UserPort::where('user_id', $user->id)->orderBy('symbol', 'asc')->get();
+        //dd($stocks[0]->symbol()->first()->);
+        return view('home', compact('stocks', 'user'));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,13 +5,48 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
+                <div class="panel-heading">Summary</div>
 
                 <div class="panel-body">
-                    You are logged in!
+                    <span>Portfolio Value: {{ $user->port_value }} บาท</span><br>
+                    <span>Cash: {{ $user->cash }} บาท</span>
                 </div>
             </div>
         </div>
+        <table class="table table-hover">
+            <thead>
+                <tr>
+                    <th>สัญลักษณ์</th>
+                    <th>ตลาด</th>
+                    <th>จำนวน</th>
+                    <th>ราคาปัจจุบัน</th>
+                    <th>มูลค่ารวม</th>
+                    <th>ซื้อ / ขาย</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($stocks as $stock)
+                    <tr>
+                        <td>{{ $stock->symbol }}</td>
+                        <td>{{ $stock->symbol()->first()->market }}</td>
+                        <td>{{ $stock->volume }}</td>
+                        @if ($stock->symbol()->first()->is_suspended)
+                            <td> - </td>
+                            <td> - </td>
+                            <td><a href="#" class="btn btn-default" disabled>การซื้อขายถูกระงับ</a></td>
+                        @else
+                            <td>{{ $stock->symbol()->first()->close_price }}</td>
+                            <td>{{ $stock->symbol()->first()->close_price * $stock->volume }}</td>
+                            @if ($stock->symbol()->first()->close_price < 10)
+                                <td><a href="{{ url('/symbol/'.$stock->symbol) }}" class="btn btn-default">ขาย</a></td>
+                            @else
+                                <td><a href="{{ url('/symbol/'.$stock->symbol) }}" class="btn btn-default">ซื้อ / ขาย</a></td>
+                            @endif
+                        @endif
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
     </div>
 </div>
 @endsection
